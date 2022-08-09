380fab9513efce1d532245f75e4d196df9e173eb
akshaykmus <akshaykmus@gmail.com>
2022-08-09 12:03:45 -0400

    Created the contact page and fixed errors.

--- frontend/src/pages/Contactus/Contactus.js
+++ frontend/src/pages/Contactus/Contactus.js
@@ -1,79 +1,152 @@
-import React, { useState } from 'react'
-import { Image } from "@chakra-ui/react"
-import { Helmet } from 'react-helmet';
+import React, { useState } from "react";
+import {
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  Image,
+} from "@chakra-ui/react";
+import { Helmet } from "react-helmet";
 
-import cover from './cover.jpg'
-import { Input,InputGroup, InputLeftElement, Textarea,Button  } from "@chakra-ui/react"
-import {BsEnvelope, GiPositionMarker, HiOutlinePhone} from 'react-icons/all'
-import './contactuscss.css'
+import cover from "./cover.jpg";
+import {
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Textarea,
+  Button,
+} from "@chakra-ui/react";
+import { BsEnvelope, GiPositionMarker, HiOutlinePhone } from "react-icons/all";
+import "./contactuscss.css";
+import { Form } from "react-bootstrap";
 const Contactus = () => {
-    const [email, setemail] = useState('')
-    const [body, setbody] = useState('')
+  const [email, setemail] = useState("");
+  const [body, setbody] = useState("");
 
-    const handlesubmit = () =>{
-        window.open(`mailto:abdessamadbouthjoul@gmail.com?subject=Sample&body=${body}`)
+  let handleInputChange = (e) => setbody(e.target.value);
+  let isEmail = false;
+  let isbody = false;
+  function ValidateEmail(email) {
+    console.log("In validate");
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+      isEmail = true;
     }
-    return (
+  }
+  function Validatemessage(body) {
+    console.log("In body validate");
+    if (body == "") {
+      isbody = false;
+    } else {
+      isbody = true;
+    }
+  }
 
-        <div className="contactUs">
-            <Helmet>
-                <title>Contact</title>
-            </Helmet>
-        <div className="headerContact">
-            <Image className="imageContact" src='https://i.imgur.com/7rwaigw.jpg' alt="contactImage" objectFit="cover"/>
-            <div className="text">
-                <h2>Contact</h2>  
-            </div>
-              
+  const handlesubmit = () => {
+    ValidateEmail(email);
+    Validatemessage(body);
+    if (isEmail == true && isbody == true) {
+      console.log(body);
+      window.open(`mailto:akshaykmus@gmail.com?subject=Sample&body=${body}`);
+    } else if (isEmail == false) {
+      //res.status(400);
+      console.log("Email is false");
+      alert("Please enter a valid email");
+    } else if (isbody == false) {
+      alert("Please enter message");
+    }
+  };
+
+  return (
+    <div className="contactUs">
+      <Helmet>
+        <title>Contact</title>
+      </Helmet>
+      <div className="headerContact">
+        <Image
+          className="imageContact"
+          src="https://i.imgur.com/7rwaigw.jpg"
+          alt="contactImage"
+          objectFit="cover"
+        />
+        <div className="text">
+          <h2>Contact</h2>
         </div>
+      </div>
 
-        <div className="card-contact">
-            <div className="sendMsg">
-                <h4>Send Us A Message</h4>
-                    <div className="inputContact">
-                        <InputGroup width="450px" >
-                            <InputLeftElement       pointerEvents="none"  children={<BsEnvelope className = 'envolope' color="gray.300" />}/>
-                            <Input value = {email} onChange = {e=> setemail(e.target.value)} type="text" placeholder="Your Email Address"/>
-                        </InputGroup>
-                        
-                    </div>
-                    <div className="textAreaCnt">
-                        <Textarea value = {body} onChange = {e=> setbody(e.target.value)} width="450px"  placeholder="How Can We Help" height="200px" />
-                    </div>
-                    <div className="contentContact">
-                        <Button onClick = {handlesubmit} borderRadius="90px" colorScheme="teal" variant="solid" size="180px" className="contactBtn">Submit</Button>
-                    </div>
-
-            </div>
-            <div className="showAdrss">
-                <div className="box">
-                    <div className="iconCtn"><GiPositionMarker opacity="0.8"/></div>
-                    <div className="adressCtn">
-
-                        <h3> Address</h3>
-                        <p>Coza Store Center 8th floor, 379 Hudson St, New York, NY 10018 US</p>
-                    </div>
-                </div>
-                    <div className="box">
-                    <div className="iconCtn"><HiOutlinePhone opacity="0.8"/></div>
-                    <div className="adressCtn">
-
-                        <h3>Lets Talk</h3>
-                        <p className="infoCtn">0657964665</p>
-                    </div>
-                </div>
-                <div className="box">
-                    <div className="iconCtn"><BsEnvelope opacity="0.8"/></div>
-                    <div className="adressCtn">
-
-                        <h3>Sale Support</h3>
-                        <p className="infoCtn">Store@gmail.com</p>
-                    </div>
-                </div>
-            </div>
+      <div className="card-contact">
+        <div className="sendMsg">
+          <h4>Send Us A Message</h4>
+          <div className="inputContact">
+            <InputGroup width="450px">
+              <InputLeftElement
+                pointerEvents="none"
+                children={<BsEnvelope className="envolope" color="gray.300" />}
+              />
+              <Input
+                value={email}
+                onChange={(e) => setemail(e.target.value)}
+                type="text"
+                placeholder="Your Email Address"
+              />
+            </InputGroup>
+          </div>
+          <div className="textAreaCnt">
+            <Textarea
+              value={body}
+              onChange={handleInputChange}
+              width="450px"
+              placeholder="How Can We Help"
+              height="200px"
+              required
+            />
+          </div>
+          <div className="contentContact">
+            <Button
+              onClick={handlesubmit}
+              borderRadius="90px"
+              colorScheme="teal"
+              variant="solid"
+              size="180px"
+              className="contactBtn"
+            >
+              Submit
+            </Button>
+          </div>
         </div>
-</div>
-    )
-}
+        <div className="showAdrss">
+          <div className="box">
+            <div className="iconCtn">
+              <GiPositionMarker opacity="0.8" />
+            </div>
+            <div className="adressCtn">
+              <h3> Address</h3>
+              <p>
+                Coza Store Center 8th floor, 379 Hudson St, New York, NY 10018
+                US
+              </p>
+            </div>
+          </div>
+          <div className="box">
+            <div className="iconCtn">
+              <HiOutlinePhone opacity="0.8" />
+            </div>
+            <div className="adressCtn">
+              <h3>Lets Talk</h3>
+              <p className="infoCtn">0657964665</p>
+            </div>
+          </div>
+          <div className="box">
+            <div className="iconCtn">
+              <BsEnvelope opacity="0.8" />
+            </div>
+            <div className="adressCtn">
+              <h3>Sale Support</h3>
+              <p className="infoCtn">Store@gmail.com</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Contactus
+export default Contactus;
