626395c192a00ab8d06ecb79c2fdc7c2e36e3314
Jayanth Vakkalagadda <vakkalagadda.j@northeastern.edu>
2022-08-10 13:24:07 -0400

    font changes

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -102,8 +102,8 @@ import Searchnav from './Searchnav';
             <ul className="navLinks" ref= {navLinks}>
                 <NavLink to="/" exact  activeClassName='activlink' ><li>Home</li></NavLink>
                 <NavLink to="/shop" activeClassName='activlink' ><li>Shop</li></NavLink>
-                <NavLink to="/contactus"activeClassName='activlink' ><li>Contact us</li></NavLink>
-                <NavLink to="/about" activeClassName='activlink'><li>About</li></NavLink>  
+                <NavLink to="/about" activeClassName='activlink'><li>About</li></NavLink>
+                <NavLink to="/contactus"activeClassName='activlink' ><li>Contact</li></NavLink>
             </ul>
             <div className="burger" ref= {Buric} onClick = {onBurgActive}>
                 <div className='line1'></div>
